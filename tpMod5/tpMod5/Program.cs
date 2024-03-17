e4be005e526ce692817cdddabc8b4f45de763e25
AdsGab <gabrielleadsf@gmail.com>
2024-03-17 11:26:17 +0700

    Menambahkan class Halo Generic dengan method SapaUser

--- tpMod5/tpMod5/Program.cs
+++ tpMod5/tpMod5/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.AccessControl;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String nama = "Gabrielle";
+        HaloGeneric.SapaUser(nama);
+    }
+}
+
+public class HaloGeneric
+{
+    public static void SapaUser<T> (T param1)
+    {
+        Console.WriteLine ("Halo user "+ param1);
+    }
+}
